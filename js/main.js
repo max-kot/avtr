b42eee8f0f518904c5fa2c55d59eb8f907bc018e
sim-max <101747552+sim-max@users.noreply.github.com>
2022-06-20 15:28:38 +0300

    added first section + concept section and menu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
-$(function(){
+$(function () {
 
 });
+
+const menuBtn = document.querySelector('.menu-btn');
+const menuList = document.querySelector('.menu__list');
+
+menuBtn.addEventListener('click', function () {
+	menuList.classList.toggle('active');
+})
