e346bf01ba5f67baf391fac32d58df38bc0547ce
sim-max <101747552+sim-max@users.noreply.github.com>
2022-06-21 15:13:09 +0300

    added all the section

--- js/main.js
+++ js/main.js
@@ -1,10 +1,31 @@
 $(function () {
-
+	$('.slider__list').slick({
+		infinite: true,
+		slidesToShow: 4,
+		variableWidth: true,
+		prevArrow: '<img src="img/design/slider/prev.svg" alt="" class="slider__prev">',
+		nextArrow: '<img src="img/design/slider/next.svg" alt="" class="slider__next">',
+		autoplay: true,
+		autoplaySpeed: 3000,
+	}
+	);
 });
 
+/*--- Меню ---*/
 const menuBtn = document.querySelector('.menu-btn');
 const menuList = document.querySelector('.menu__list');
 
 menuBtn.addEventListener('click', function () {
 	menuList.classList.toggle('active');
+})
+
+/*--- Кнопка на видео---*/
+const videoPlayer = document.querySelector('.video-block__video');
+const videoPlayBtn = document.querySelector('.video-block__button');
+
+videoPlayBtn.addEventListener('click', function () {
+	if (videoPlayer.play()) {
+		videoPlayer.controls = true;
+	}
+
 })
